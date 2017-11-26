94f92db8c089c2dd781af8de8d13d5a73d5ddf5a
Marc <marc.friedrich94@web.de>
2017-11-26 20:41:08 +0100

    update skripte

--- config_tables.sql
+++ config_tables.sql
@@ -1,2 +1,3 @@
 ALTER TABLE tralala_users ADD FOREIGN KEY (pid) REFERENCES tralala_profiles(pid) ON DELETE CASCADE;
 ALTER TABLE tralala_posts ADD FOREIGN KEY (uid) REFERENCES tralala_users(uid) ON DELETE NO ACTION;
+ALTER TABLE tralala_users ADD FOREIGN KEY (role_id) REFERENCES tralala_roles(role_id) ON DELETE NO ACTION;
